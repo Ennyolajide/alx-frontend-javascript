2b490f74015020787c7159cc8dbd9696ff2b4c08
Olajide Eniseyin <eniseyinolajide@gmail.com>
2023-07-05 04:06:07 +0100

    Combine2

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,3 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-        .filter(student => student.location === city)
-        .map(student => {
-            const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
-            const grade = matchingGrade ? matchingGrade.grade : 'N/A';
-            return { ...student, grade };
-        });
-}
+    return students.filter((std) => std.location === city).map((std) => ({ ...std, grade: newGrades.filter((g) => g.studentId === std.id).map((g) => g.grade)[0] || 'N/A' }));
+  }
