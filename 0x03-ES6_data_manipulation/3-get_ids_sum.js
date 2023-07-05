798b5b9789b605298ce390c91c9bea5a2a587d10
Olajide Eniseyin <eniseyinolajide@gmail.com>
2023-07-05 10:43:30 +0100

    Complete

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,5 @@
-export default function getStudentIdsSum(students) {
-    return Array.isArray(students) ? students.reduce((accumulator, currentValue) => accumulator + currentValue, 0) : 0;
+function getStudentIdsSum(students) {
+    students.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
+
+export default getStudentIdsSum;
