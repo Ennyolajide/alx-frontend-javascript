a8b7368e436fa4a803661d8e072f33cfad0ded18
Olajide Eniseyin <eniseyinolajide@gmail.com>
2023-07-08 04:33:42 +0100

    Complete

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,15 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  let queryCount = weakMap.get(endpoint) || 0;
-  queryCount++;
-  weakMap.set(endpoint, queryCount);
+  let called = weakMap.get(endpoint) || 0;
 
-  if (queryCount >= 5) {
-    throw new Error('Endpoint load is high');
+  called += 1;
+
+  weakMap.set(endpoint, called);
+
+  if (called >= 5) {
+    throw Error('Endpoint load is high');
   }
+
+  return called;
 }
